159432a38c4f400ef993e36b74352b9d2f48e156
hshen12 <hshen12@dons.usfca.edu>
2017-11-17 01:37:46 -0800

    Add files via upload

---
+++ Hashtable.java
@@ -0,0 +1,77 @@
+
+public class Hashtable {
+
+	class HashNode {
+		private String key;
+		private String value;
+		private HashNode next;
+		
+		public HashNode(String key, String value) {
+			this.key = key;
+			this.value = value;
+			this.next = null;
+		}
+	}
+	
+	private HashNode[] arr;
+	
+	public Hashtable() {
+		this.arr = new HashNode[10000];
+
+	}
+	
+	public void put(String key, String val) {
+		int position = Math.abs(key.hashCode()) % arr.length;
+		HashNode head = arr[position];
+		HashNode node = new HashNode(key, val);
+		node.next = head;
+		arr[position] = node;
+		
+	}
+
+	public Object get(String key) {
+		int position = Math.abs(key.hashCode()) % arr.length;
+		HashNode head  = arr[position];
+		if(head == null) {
+			return null;
+		}
+		while(head != null) {
+			if(head.key.equals(key)) {
+				return head.value;
+			}
+			head = head.next;
+		}
+		return null;
+	}
+
+	public String remove(String key) {
+		int position = Math.abs(key.hashCode()) % arr.length;
+		HashNode head = arr[position];
+		if(head == null) {
+			return null;
+		}
+		
+		if(head.key.equals(key)) {
+			arr[position] = head.next;
+			return head.value;
+		}
+		
+		HashNode prev = head;
+		while(head != null) {
+			if(head.key.equals(key)) {
+				prev.next = head.next;
+				return head.value;
+			}
+			prev = head;
+			head = head.next;
+		}
+		return null;
+		
+		
+	}
+
+	public boolean containsKey(String key) {
+		return arr[Math.abs(key.hashCode()) % arr.length] != null;
+	}
+
+}
